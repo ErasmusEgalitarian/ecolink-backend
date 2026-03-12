3a71f3c65431427ed239c31cd5b9cdf937616608
Pedro Lucas Dourado <pedrolucas@hotmail.com>
2026-03-12 15:58:08 -0300

    feat: add EcoPoint model and updated/added the ecopointId on donation and donationController

--- models/Donation.js
+++ models/Donation.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const DonationSchema = new mongoose.Schema({
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    ecopointId: { type: String, required: true }, // Change to reference the Ecopoint model when it's available
+    ecopointId: { type: mongoose.Schema.Types.ObjectId, ref: 'EcoPoint', required: true }, // Change to reference the Ecopoint model when it's available
     donationDate: { type: Date, default: Date.now },
     materialType: { type: String, enum: ['plastic', 'metal', 'glass', 'paper'], required: true },
     description: { type: String, default: '' },
